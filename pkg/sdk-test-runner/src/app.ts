affafa91726561918d73cc930879231731fb540e
Ty Potter <tyler@fluxon.com>
2024-09-06 13:01:58 -0600

    flag tests running

--- pkg/sdk-test-runner/src/app.ts
+++ pkg/sdk-test-runner/src/app.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 import config from './config';
 import * as fs from 'fs';
 import path from 'path';
+import { green, red, yellow } from './logging';
+
 
 console.log(config.logPrefix + `Firing up test runner for ${config.sdkName}`);
 console.log(config.logPrefix + `Posting test cases to SDK server at ${config.sdkServer}`);
@@ -11,9 +13,9 @@ const testConfig = JSON.parse(fs.readFileSync(config.scenarioFile, 'utf-8'));
 
 const scenarios = testConfig['scenarios'];
 
-const failures: Record<string, object>[] = [];
-const testCases = 0;
-const resultSets: Record<string, object>[] = [];
+const failures: Record<string, any>[] = [];
+let numTestCases = 0;
+// failures.push({error: "I'm a fake failure"});
 
 const testScenario = async (key: string) => {
     const safeSdkName = encodeURIComponent(config.sdkName);
@@ -41,46 +43,67 @@ const testScenario = async (key: string) => {
             const testCase = fs.readFileSync(filePath, "utf-8");
 
             const testCaseObj = JSON.parse(testCase);
-            // Prep the expected answers
-            const assignments = Object.keys(testCaseObj['subjects']).map(subject => {
-                return testCaseObj['subjects'][subject]['assignment'];
-            });
 
-            // Post the test case to the SDK relay 
             // Determine whether case is a flag assignment or bandit selection
 
-            const results = await axios.post(`${config.sdkServer}/flags/v1/assignments`, testCaseObj).then(
-                result => {
-                    console.log(result);
-                    return result.data;
+            // Flag testing!!
+            if (!!testCaseObj['variationType']) {
+
+                // Loop through the subjects and get their assignments.
+
+                for (let subject of testCaseObj['subjects']) {
+                    const flagKey = testCaseObj['flag'];
+                    const variationType = testCaseObj['variationType'];
+                    const defaultValue = testCaseObj['defaultValue'];
+                    const subjectKey = subject['subjectKey'];
+                    const payload = {
+                        flag: flagKey,
+                        variationType: variationType,
+                        defaultValue: defaultValue,
+                        subjectKey: subjectKey,
+                        subjectAttributes: subject['subjectAttributes']
+                    };
+
+                    numTestCases++;
+
+                    // Post the test case to the SDK relay 
+
+                    const results = await axios.post(`${config.sdkServer}/flags/v1/assignment`, payload).then(
+                        result => {
+                            return result.data;
+                        }
+                    ).catch(error => {
+                        console.error('Error:', error);
+                        failures.push({ error });
+                    });
+
+                    const passed = isResultCorrect(results, subject);
+
+                    if (!passed) {
+                        failures.push(results);
+                    }
+
+                    console.log(config.logPrefix + (passed ? green("pass") : red("fail")) + ` ${flagKey}[${subjectKey}] `);
                 }
-            ).catch(error => {
-                console.error('Error:', error);
-            });
-
-            console.log({ assignments, results });
-
-
-            // Compare the results.
-            const failed = JSON.stringify(assignments) == JSON.stringify(results);
-            if (failed) {
-                failures.push(results);
             }
-            resultSets.push(results);
-
-            // console.log(testCaseObj);
-            // console.log(assignments);
         }
-        break;
     }
-    console.log({ failutes: failures, resultSets });
-};
+}
 
 (async () => {
     for (const scenario of Object.keys(scenarios)) {
         await testScenario(scenario);
     }
 })().then(() => {
+
+    const passes = numTestCases - failures.length;
+    console.log(config.logPrefix + yellow("*** Test Results *** "));
+    console.log(config.logPrefix + green(`${passes}/${numTestCases} passed`));
+    console.log(config.logPrefix + red(`${failures.length} failures`));
+    if (failures.length) {
+        console.log(config.logPrefix + red("Failure Details"));
+    }
+    console.log(failures);
 
     // Print results summary
 
@@ -92,3 +115,10 @@ const testScenario = async (key: string) => {
     }
 }
 );
+
+function isResultCorrect(results: any, subject: any): boolean {
+    if (subject['assignment'] == results['assignment'] || subject['assignment'] == results['result']) {
+        return true;
+    }
+    return false;
+}
