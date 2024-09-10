6ecbddbc7038afc7c04eaf3a7e15f74d2ef63480
Ty Potter <tyler@fluxon.com>
2024-09-10 14:40:59 -0600

    test data clone script

--- package-testing/testing-api/clone-test-data.sh
+++ package-testing/testing-api/clone-test-data.sh
@@ -1,9 +1,13 @@
-TEST_DATA_REF=${TEST_DATA_REF:-main}
+TEST_DATA_REF=${1:-main}
+
 mkdir -p test-data
 cd test-data
 git clone -b $TEST_DATA_REF --depth 1 --single-branch https://github.com/Eppo-exp/sdk-test-data.git tmp
+
+# copy test files (config and test cases)
 cp -R tmp/ufc ./
 
-rm -Rf tmp
+# copy the scenarios file.
+cp tmp/package-testing/scenarios.json ./
 
-cp ../../scenarios.json ./ # TODO copy from the repository instead of locally.
+rm -Rf tmp
