9a829231555212104ec84d5b665de6a1c7e56e73
Ty Potter <tyler@fluxon.com>
2024-09-12 15:31:47 -0600

    docker run fix

--- package-testing/php-sdk-relay/docker_run.sh
+++ package-testing/php-sdk-relay/docker_run.sh
@@ -3,4 +3,6 @@ source .env;
 docker run --env-file ./.env \
   -p $SDK_RELAY_PORT:$SDK_RELAY_PORT \
   -e SDK_REF \
-  -t Eppo/php-sdk-test
+  --name php-relay \
+  --network="host" \
+  -t Eppo-exp/php-sdk-relay
