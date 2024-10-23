073afbbe73007ee6a5b1f13f29765eef0e98abf1
Tyler Potter <tyler.potter@geteppo.com>
2024-10-23 14:54:21 -0600

    PHP SDK Relay Server (#62)
    * PHP testing app
    * script to build and launch using a ref of the SDK
    * Dockeration
    * release script
    * healthcheck
    * readme

---
+++ package-testing/php-sdk-relay/docker-run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Default is to use the latest build
+VERSION="${1:-latest}"
+
+if [ -e .env ]; then
+  source .env
+fi
+
+docker stop php-relay
+docker remove php-relay
+docker run  -p $SDK_RELAY_PORT:$SDK_RELAY_PORT \
+  -e SDK_REF \
+  -e SDK_RELAY_PORT \
+  --name php-relay \
+  -d \
+  -t Eppo-exp/php-sdk-relay:$VERSION && \
+  echo "Container running"
