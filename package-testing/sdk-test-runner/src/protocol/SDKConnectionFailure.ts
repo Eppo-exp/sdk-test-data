21911112c25b1ab3c5a226215fd23e376b300b86
Tyler Potter <tyler.potter@geteppo.com>
2024-11-04 10:02:21 -0700

    Feat: Test SDK Client relays (Android, ios, etc.) with the common test runner (#69)
    * prep for client mode, extract SDK relay interface
    * update docs

---
+++ package-testing/sdk-test-runner/src/protocol/SDKConnectionFailure.ts
@@ -0,0 +1,3 @@
+export type SDKConnectionFailure = {
+  errorMessage?: string;
+};
---
+++ package-testing/sdk-test-runner/src/protocol/SDKRelay.ts
@@ -0,0 +1,17 @@
+import { AssignmentRequest } from '../dto/assignmentRequest';
+import { BanditActionRequest } from '../dto/banditActionRequest';
+import { TestResponse } from '../dto/testResponse';
+import { SDKConnectionFailure } from './SDKConnectionFailure';
+import { SDKInfo } from './SDKInfo';
+
+/**
+ * Wrapper for calls to an SDK relay
+ */
+
+export type SDKRelay = {
+  isReady(timeout: number): Promise<SDKInfo | SDKConnectionFailure>;
+  reset(): Promise<void>;
+  getAssignment(request: AssignmentRequest): Promise<TestResponse>;
+  getBanditAction(request: BanditActionRequest): Promise<TestResponse>;
+  close(): void;
+};
