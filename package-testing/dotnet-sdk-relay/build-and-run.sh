75f99f03e2ff42a9736e9c69da5c09f7a747dd02
Ty Potter <tyler@fluxon.com>
2024-11-12 10:20:07 -0700

    wip

--- package-testing/dotnet-sdk-relay/build-and-run.sh
+++ package-testing/dotnet-sdk-relay/build-and-run.sh
@@ -1,10 +1,62 @@
 #!/usr/bin/env bash
 
+# One of macos, ubuntu, windows
+PLATFORM=${PLATFORM:=ubuntu}
+DOTNET_VERSION="8.0"
+
 if [ -e .env ]; then
   source .env
 fi
 
-# TODO: Substitute the specified version or REF of the SDK
+
+# Configure environment to **build** the app
+
+# Github runners use the package managers below
+# https://github.com/actions/runner-images
+case "${PLATFORM}" in
+    "windows")
+        choco install dotnet-sdk -v ${DOTNET_VERSION}
+        ;;
+    "macos")
+        brew install dotnet-sdk@${DOTNET_VERSION}
+        ;;
+    "ubuntu")
+        sudo apt update
+        sudo apt install dotnet-sdk-${DOTNET_VERSION}
+        ;;
+    *)
+        echo "Unsupported platform: ${PLATFORM}"
+        exit 1
+        ;;
+esac
+
+
+
+# Inject desired SDK version
 
 dotnet build EppoSDKRelay
+dotnet publish
+  
+
+
+# Configure environment to **run** the app
+case "${PLATFORM}" in
+    "Windows")
+        choco install dotnet-runtime -v ${DOTNET_VERSION}
+        ;;
+    "Darwin")
+        brew install dotnet
+        ;;
+    "Linux")
+        sudo apt update
+        sudo apt install dotnet
+        ;;
+    *)
+        echo "Unsupported platform: ${PLATFORM}"
+        exit 1
+        ;;
+esac
+
+
+
 dotnet run --project EppoSDKRelay
