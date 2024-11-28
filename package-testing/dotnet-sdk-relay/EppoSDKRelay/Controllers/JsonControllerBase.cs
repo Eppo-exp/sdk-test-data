92cb5bcf2f343d6d7546697b87331aeaed5d8551
Ty Potter <tyler@fluxon.com>
2024-11-28 13:38:41 -0700

    Return assignment and bandit logs in test responses

--- package-testing/dotnet-sdk-relay/EppoSDKRelay/Controllers/JsonControllerBase.cs
+++ package-testing/dotnet-sdk-relay/EppoSDKRelay/Controllers/JsonControllerBase.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using EppoSDKRelay.DTO;
 using System.Text.Json;
 using Newtonsoft.Json.Linq;
+using eppo_sdk.dto;
+using eppo_sdk.dto.bandit;
 
 namespace EppoSDKRelay.controllers;
 
-public class JsonControllerBase : ControllerBase {
+public class JsonControllerBase : ControllerBase
+{
 
     protected static readonly JsonSerializerOptions SerializeOptions = new()
     {
@@ -14,7 +17,26 @@ public class JsonControllerBase : ControllerBase {
         WriteIndented = true
     };
 
-    protected static ActionResult<string> JsonResult(object result)
+    protected static ActionResult<string> JsonTestResponse(object result)
+    {
+        // System.Text.Json does not play nicely with Newtonsoft types
+        // Since "Objects" implement IEnumerable, System.Text will try to encode
+        // the json object as an array. :(
+        if (result is JObject)
+        {
+            result = ((JObject)result).ToObject<Dictionary<string, object>>();
+        }
+
+        var response = new TestResponse
+        {
+            Result = result,
+            AssignmentLog = DequeueAll<AssignmentLogData>(AssignmentLogger.Instance.AssignmentLogs),
+            BanditLog =  DequeueAll<BanditLogEvent>(AssignmentLogger.Instance.BanditLogs),
+        };
+        return JsonSerializer.Serialize(response, SerializeOptions);
+    }
+
+    protected static ActionResult<string> JsonObjectResponse(object result)
     {
         // System.Text.Json does not play nicely with Newtonsoft types
         // Since "Objects" implement IEnumerable, System.Text will try to encode
@@ -22,12 +44,7 @@ public class JsonControllerBase : ControllerBase {
         if (result is JObject) {
             result = ((JObject)result).ToObject<Dictionary<string, object>>();
         }
-
-        var response = new TestResponse
-        {
-            Result = result
-        };
-        return JsonSerializer.Serialize(response, SerializeOptions);
+        return JsonSerializer.Serialize(result, SerializeOptions);
     }
     
     protected static ActionResult<string> JsonError(String error)
@@ -37,5 +54,21 @@ public class JsonControllerBase : ControllerBase {
             Error = error
         }, SerializeOptions);
     }
-    
+
+
+    public static List<object> DequeueAll<T>(Queue<T> queue)
+    {
+        List<object> items = [];
+
+        while (queue.Count > 0)
+        {
+            var item = queue.Dequeue();
+            if (item != null)
+            {
+                items.Add(item);
+            }
+        }
+
+        return items;
+    }
 }
