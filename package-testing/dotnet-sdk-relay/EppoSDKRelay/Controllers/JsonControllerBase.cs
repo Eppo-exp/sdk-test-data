7a62c9fc42f13b7ccd007d77ceffbf2e0a8278a7
Tyler Potter <tyler.potter@geteppo.com>
2024-12-05 10:40:56 -0700

    fix: bandit attributes, logs, and baseUrl in dotnet SDK relay (#85)
    * Return assignment and bandit logs in test responses
    * rename json test response method
    * use latest SDK package, update Program hosts
    * use Base URL instead of host/port
    * handle input attributes without losing kind (numeric/categorical)

--- package-testing/dotnet-sdk-relay/EppoSDKRelay/Controllers/JsonControllerBase.cs
+++ package-testing/dotnet-sdk-relay/EppoSDKRelay/Controllers/JsonControllerBase.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using EppoSDKRelay.DTO;
 using System.Text.Json;
 using Newtonsoft.Json.Linq;
+using eppo_sdk.dto;
+using eppo_sdk.dto.bandit;
 
 namespace EppoSDKRelay.controllers;
 
-public class JsonControllerBase : ControllerBase {
+public class JsonControllerBase : ControllerBase
+{
 
     protected static readonly JsonSerializerOptions SerializeOptions = new()
     {
@@ -14,22 +17,25 @@ public class JsonControllerBase : ControllerBase {
         WriteIndented = true
     };
 
-    protected static ActionResult<string> JsonResult(object result)
+    protected static ActionResult<string> JsonTestResponse(object result)
     {
         // System.Text.Json does not play nicely with Newtonsoft types
         // Since "Objects" implement IEnumerable, System.Text will try to encode
         // the json object as an array. :(
-        if (result is JObject) {
+        if (result is JObject)
+        {
             result = ((JObject)result).ToObject<Dictionary<string, object>>();
         }
 
         var response = new TestResponse
         {
-            Result = result
+            Result = result,
+            AssignmentLog = DequeueAllForResponse<AssignmentLogData>(AssignmentLogger.Instance.AssignmentLogs),
+            BanditLog =  DequeueAllForResponse<BanditLogEvent>(AssignmentLogger.Instance.BanditLogs),
         };
         return JsonSerializer.Serialize(response, SerializeOptions);
     }
-    
+
     protected static ActionResult<string> JsonError(String error)
     {
         return JsonSerializer.Serialize(new TestResponse
@@ -37,5 +43,20 @@ public class JsonControllerBase : ControllerBase {
             Error = error
         }, SerializeOptions);
     }
-    
+
+    public static List<object> DequeueAllForResponse<T>(Queue<T> queue)
+    {
+        List<object> items = [];
+
+        while (queue.Count > 0)
+        {
+            var item = queue.Dequeue();
+            if (item != null)
+            {
+                items.Add(item);
+            }
+        }
+
+        return items;
+    }
 }
