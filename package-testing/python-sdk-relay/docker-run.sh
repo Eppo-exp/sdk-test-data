873e99ad296740f346b916ed9d8545375169bfb5
Leo Romanovsky <leo@geteppo.com>
2024-12-07 08:53:16 -0800

    python packaged server tests

---
+++ package-testing/python-sdk-relay/docker-run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Default is to use the latest build
+VERSION="${1:-latest}"
+
+if [ -e .env ]; then
+  source .env
+fi
+
+docker stop python-relay
+docker remove python-relay
+
+docker build . -t Eppo-exp/python-sdk-relay:$VERSION
+
+docker run  -p $SDK_RELAY_PORT:$SDK_RELAY_PORT \
+  --add-host host.docker.internal:host-gateway \
+  -e SDK_REF \
+  -e EPPO_BASE_URL \
+  -e SDK_RELAY_PORT \
+  --name python-relay \
+  -d --rm \
+  -t Eppo-exp/python-sdk-relay:$VERSION && \
+  echo "Container running"
---
+++ package-testing/python-sdk-relay/src/server.py
@@ -0,0 +1,9 @@
+import time
+
+def main():
+    while True:
+        print("Hello, world!")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
