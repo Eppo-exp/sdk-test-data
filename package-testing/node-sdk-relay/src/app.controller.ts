3487e0799fe142dd4084df7d68231c34e56beef3
selki <maya.nguyen@fluxon.com>
2024-12-12 13:29:13 +0000

    get assignment handling proxy

--- package-testing/node-sdk-relay/src/app.controller.ts
+++ package-testing/node-sdk-relay/src/app.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AssignmentDto } from './types';
 import { EppoClientProxy } from './eppoClientProxy';
+import { getInstance } from '@eppo/node-server-sdk';
 
 @Controller()
 export class AppController {
@@ -15,7 +16,7 @@ export class AppController {
 
   @Post('flags/v1/assignment')
   getAssignment(@Body() requestedAssignmentBody: AssignmentDto) {
-    const assignment = this.eppoClientProxy.getAssignment(requestedAssignmentBody);
+    const assignment = this.eppoClientProxy.getAssignment(getInstance(), requestedAssignmentBody);
     console.log(assignment);
     return 'ok';
   }
