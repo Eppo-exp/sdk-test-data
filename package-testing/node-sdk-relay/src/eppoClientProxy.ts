ab8b13503d88e90be233aedac177b6c25c159be7
selki <maya.nguyen@fluxon.com>
2025-01-07 18:16:13 +0100

    get bandits handling proxy

--- package-testing/node-sdk-relay/src/eppoClientProxy.ts
+++ package-testing/node-sdk-relay/src/eppoClientProxy.ts
@@ -1,31 +1,74 @@
-import { AssignmentDto } from './types';
+import { AssignmentDto, BanditDto } from './types';
 import { EppoClient } from '@eppo/js-client-sdk-common';
 import getLogger from './main';
 
+export enum AssignmentTypes {
+  INTEGER_ASSIGNMENT_TYPE = 'INTEGER',
+  STRING_ASSIGNMENT_TYPE = 'STRING',
+  BOOLEAN_ASSIGNMENT_TYPE = 'BOOLEAN',
+  NUMERIC_ASSIGNMENT_TYPE = 'NUMERIC',
+  JSON_ASSIGNMENT_TYPE = 'JSON',
+}
+
+type AssignmentResult = number | string | object | boolean;
+
 export class EppoClientProxy {
   private readonly assignmentTypeToMethod = new Map([
-    ['INTEGER', 'getIntegerAssignment'],
-    ['STRING', 'getStringAssignment'],
-    ['BOOLEAN', 'getBooleanAssignment'],
-    ['NUMERIC', 'getNumericAssignment'],
-    ['JSON', 'getJSONAssignment'],
+    [AssignmentTypes.INTEGER_ASSIGNMENT_TYPE, 'getIntegerAssignment'],
+    [AssignmentTypes.STRING_ASSIGNMENT_TYPE, 'getStringAssignment'],
+    [AssignmentTypes.BOOLEAN_ASSIGNMENT_TYPE, 'getBooleanAssignment'],
+    [AssignmentTypes.NUMERIC_ASSIGNMENT_TYPE, 'getNumericAssignment'],
+    [AssignmentTypes.JSON_ASSIGNMENT_TYPE, 'getJSONAssignment'],
   ]);
+
+  private castAssignmentResult(result, assignmentType: AssignmentTypes) {
+    switch (assignmentType) {
+      case AssignmentTypes.INTEGER_ASSIGNMENT_TYPE:
+        return parseInt(result, 10);
+      case AssignmentTypes.STRING_ASSIGNMENT_TYPE:
+        return result;
+      case AssignmentTypes.BOOLEAN_ASSIGNMENT_TYPE:
+        return Boolean(result);
+      case AssignmentTypes.NUMERIC_ASSIGNMENT_TYPE:
+        return Number(result);
+      case AssignmentTypes.JSON_ASSIGNMENT_TYPE:
+        return Object(result);
+    }
+  }
 
   getAssignment(eppoClientInstance: EppoClient, assignmentRequestBody: AssignmentDto) {
     const method = this.assignmentTypeToMethod.get(assignmentRequestBody.assignmentType);
 
-    const result = eppoClientInstance[method](
+    let result: AssignmentResult = eppoClientInstance[method](
       assignmentRequestBody.flag,
       assignmentRequestBody.subjectKey,
       assignmentRequestBody.subjectAttributes,
       assignmentRequestBody.defaultValue,
     );
+    result = this.castAssignmentResult(result, assignmentRequestBody.assignmentType);
 
     return {
       subjectKey: assignmentRequestBody.subjectKey,
-      result,
+      result: result,
       request: assignmentRequestBody,
       assignmentLog: getLogger(),
+    };
+  }
+
+  getBanditAction(eppoClientInstance: EppoClient, banditRequestBody: BanditDto) {
+    const result = eppoClientInstance.getBanditAction(
+      banditRequestBody.flag,
+      banditRequestBody.subjectKey,
+      banditRequestBody.subjectAttributes,
+      banditRequestBody.actions,
+      banditRequestBody.defaultValue,
+    );
+
+    return {
+      subjectKey: banditRequestBody.subjectKey,
+      result: result,
+      request: banditRequestBody,
+      banditLog: getLogger(),
     };
   }
 }
--- package-testing/node-sdk-relay/src/types.ts
+++ package-testing/node-sdk-relay/src/types.ts
@@ -1,10 +1,28 @@
+import { AssignmentTypes } from './eppoClientProxy';
+
 export type AttributeType = string | number | boolean;
+export type Attributes = Record<string, AttributeType>;
 export type SubjectAttributes = Record<string, AttributeType>;
+export type BanditActions = Record<string, ContextAttributes>;
+
+export type ContextAttributes = {
+  actionKey: string;
+  numericAttributes: Attributes;
+  categoricalAttributes: Attributes;
+};
 
 export class AssignmentDto {
   flag: string;
-  assignmentType: string;
+  assignmentType: AssignmentTypes;
   defaultValue: string;
   subjectKey: string;
   subjectAttributes: SubjectAttributes;
+}
+
+export class BanditDto {
+  flag: string;
+  defaultValue: string;
+  subjectKey: string;
+  subjectAttributes: SubjectAttributes;
+  actions: BanditActions;
 }
