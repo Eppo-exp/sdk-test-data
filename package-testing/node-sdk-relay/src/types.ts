3d1daccd012af6ddff0b3f0c0a50729784a5c268
selki <maya.nguyen@fluxon.com>
2025-01-24 11:28:02 +0100

    fixed type injection for node-server-sdk relay app

--- package-testing/node-sdk-relay/src/types.ts
+++ package-testing/node-sdk-relay/src/types.ts
@@ -1,28 +1,32 @@
 import { AssignmentTypes } from './eppoClientProxy';
-
-export type AttributeType = string | number | boolean;
-export type Attributes = Record<string, AttributeType>;
-export type SubjectAttributes = Record<string, AttributeType>;
-export type BanditActions = Record<string, ContextAttributes>;
-
-export type ContextAttributes = {
-  actionKey: string;
-  numericAttributes: Attributes;
-  categoricalAttributes: Attributes;
-};
+import { Attributes, BanditSubjectAttributes, BanditActions } from '@eppo/node-server-sdk';
 
 export class AssignmentDto {
   flag: string;
   assignmentType: AssignmentTypes;
   defaultValue: string;
   subjectKey: string;
-  subjectAttributes: SubjectAttributes;
+  subjectAttributes: Attributes;
 }
 
 export class BanditDto {
   flag: string;
   defaultValue: string;
   subjectKey: string;
-  subjectAttributes: SubjectAttributes;
+  subjectAttributes: BanditSubjectAttributes;
   actions: BanditActions;
 }
+
+type BanditActionsTestRunnerInput = {
+  actionKey: string;
+  numericAttributes: Attributes;
+  categoricalAttributes: Attributes;
+};
+
+export type BanditTestRunnerInput = {
+  flag: string;
+  defaultValue: string;
+  subjectKey: string;
+  subjectAttributes: BanditSubjectAttributes;
+  actions: BanditActionsTestRunnerInput[];
+};
